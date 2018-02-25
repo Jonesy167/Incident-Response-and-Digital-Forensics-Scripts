c82cb2a55924063c2a016ba839b570e67541421c
Jonesy167 <30509908+Jonesy167@users.noreply.github.com>
2018-02-25 20:19:44 +0000

    Update create hashes.bat

--- create hashes.bat
+++ create hashes.bat
@@ -1,3 +1,5 @@
+del %~d0\verify.txt
 del  %~d0\hashes.xml
-start %~d0\cmd_x64.exe /k "fciv.exe Tools -r -xml %~d0\hashes.xml"
+start %~d0\cmd_x64.exe /k "fciv.exe Tools -r -xml %~d0\hashes.xml" ::change file paths as appropriate
 /k  exit
+%~d0\cmd_x64.exe /k exit
