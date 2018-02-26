25dee27f0420ed4649c6e1ebdd1edee9101c5b9c
Jonesy167 <30509908+Jonesy167@users.noreply.github.com>
2018-02-26 13:32:37 +0000

    Update verify hashes.bat

--- verify hashes.bat
+++ verify hashes.bat
@@ -1,1 +1,1 @@
-start %~d0\cmd_x64.exe /k "fciv.exe -v -xml %~d0\hashes.xml Tools -r >> verify.txt" ::change file paths as appropriate
+start %~d0\cmd_x64.exe /k "fciv.exe -v -xml %~d0\hashes.xml Tools -r >> verify.txt"  && %~d0\cmd_x64.exe /k "type %~d0\verify.txt" 
