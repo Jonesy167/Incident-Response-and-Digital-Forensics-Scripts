58834603072b45e71c130fc3e766631efee19057
Jonesy167 <30509908+Jonesy167@users.noreply.github.com>
2018-02-26 14:05:08 +0000

    Update verify hashes.bat

--- verify hashes.bat
+++ verify hashes.bat
@@ -1,1 +1,2 @@
-start %~d0\cmd_x64.exe /k "fciv.exe -v -xml %~d0\hashes.xml Tools -r >> verify.txt"  && %~d0\cmd_x64.exe /k "type %~d0\verify.txt" 
+start %~d0\cmd_x64.exe /k "fciv.exe -v -xml %~d0\hashes.xml Tools -r >> verify.txt"  && TIMEOUT 2 && /k exit
+start %~d0\cmd_x64.exe /k "type verify.txt" 
