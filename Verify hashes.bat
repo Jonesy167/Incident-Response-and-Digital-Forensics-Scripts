770c4aeb09ba6b433bb9b484df2cc9ec6b0c8864
Jonesy167 <30509908+Jonesy167@users.noreply.github.com>
2018-03-02 15:29:53 +0000

    Delete Verify hashes.bat

--- Verify hashes.bat
+++
@@ -1,5 +0,0 @@
-start %~d0\cmd_x86.exe /k "%~d0\fciv.exe -v -xml %~d0\hashes.xml Tools -r >> %~d0\verify.txt"  && TIMEOUT 2 && /k exit
-TIMEOUT 1
-start %~d0\cmd_x86.exe /k "type %~d0\verify.txt" 
-echo test
-
